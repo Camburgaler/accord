2fedc9fce7f3383ecbe8b0871e280853845b7076
Cameron Chrobocinski <cameronchrobo@gmail.com>
2026-01-14 10:38:24 -0600

    removed live listener from DLL

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,8 @@ use eldenring::{
     util::system::wait_for_system_init,
 };
 use fromsoftware_shared::{F32Vector4, FromStatic, OwnedPtr, program::Program, task::*};
-use mio::{Events, Interest, Poll, Token, net::TcpListener, net::TcpStream};
+use mio::net::TcpStream;
 use std::{
-    ffi::CString,
-    io::{Read, Write, stdout},
     net::SocketAddr,
     sync::{
         Mutex,
@@ -18,10 +16,6 @@ use std::{
     time::{Duration, SystemTime, UNIX_EPOCH},
 };
 use telemetry::{TelemetrySocket, TelemetryV1};
-use windows_sys::Win32::System::Console::{
-    AllocConsole, ENABLE_VIRTUAL_TERMINAL_PROCESSING, GetConsoleMode, GetStdHandle,
-    STD_OUTPUT_HANDLE, SetConsoleMode, SetConsoleTitleA,
-};
 
 #[unsafe(no_mangle)]
 /// # Safety
@@ -47,62 +41,11 @@ pub unsafe extern "C" fn DllMain(_hmodule: usize, reason: u32) -> bool {
         let last_emit_ms = AtomicU64::new(0);
         let address: SocketAddr = "127.0.0.1:5555".parse().unwrap();
         let stream = TcpStream::connect(address).unwrap();
-        let mut listener = TcpListener::bind(address).unwrap();
-        let mut poll = Poll::new().unwrap();
-        let mut events = Events::with_capacity(TelemetryV1::SIZE);
-
-        unsafe {
-            AllocConsole();
-            let handle = GetStdHandle(STD_OUTPUT_HANDLE);
-            let mut mode = 0;
-            if GetConsoleMode(handle, &mut mode) != 0 {
-                SetConsoleMode(handle, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
-            }
-            let title = CString::new("Elden Ring Telemetry").unwrap();
-            SetConsoleTitleA(title.as_ptr() as _);
-        }
-
-        // Register the listener with `Poll`
-        poll.registry()
-            .register(&mut listener, Token(0), Interest::READABLE)
-            .unwrap();
 
         // Create socket
         let socket = std::sync::Arc::new(Mutex::new(TelemetrySocket::new()));
         socket.lock().unwrap().set_stream(stream);
         let socket_bg = socket.clone();
-
-        // Spawn listener thread
-        std::thread::spawn(move || {
-            loop {
-                // Poll for events
-                poll.poll(&mut events, Some(Duration::from_millis(100)))
-                    .unwrap();
-
-                for event in &events {
-                    if event.token() == Token(0) && event.is_readable() && !event.is_error() {
-                        let (mut stream, _) = listener.accept().unwrap();
-                        let mut buf = [0; TelemetryV1::SIZE];
-
-                        // Read data from the stream
-                        loop {
-                            let data = stream.read(&mut buf).unwrap();
-                            if data == 0 {
-                                break;
-                            }
-
-                            // write to console
-                            let mut stdout = stdout();
-
-                            write!(stdout, "\x1B[H{}\n\x1B[J", String::from_utf8_lossy(&buf)).ok();
-                            stdout.flush().ok();
-
-                            std::thread::sleep(Duration::from_millis(100));
-                        }
-                    }
-                }
-            }
-        });
 
         // Register a new task with the game to happen every frame during the gameloops
         // ChrIns_PostPhysics phase because all the physics calculations have ran at this
@@ -132,7 +75,7 @@ pub unsafe extern "C" fn DllMain(_hmodule: usize, reason: u32) -> bool {
                     let physics: &OwnedPtr<CSChrPhysicsModule> =
                         &player.chr_ins.module_container.physics;
 
-                    // Log position and direction.
+                    // Send position and direction.
                     let pos: HavokPosition = physics.position;
                     let euler: EulerAngles = physics.orientation.to_euler_angles();
                     let chunk: F32Vector4 = player.chr_ins.chunk_position;
